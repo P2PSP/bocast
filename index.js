91d64b4fb44f5f8d991bf4c07f9f56aca9ba97a6
Jorge García Hinestrosa <jorgegarciahinestrosa@gmail.com>
2016-02-18 14:17:57 +0100

    Implement channels.js and api.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 'use strict';
 
 var express = require('express');
-var app = express();
+var api = require('lib/api.js');
 
+var app = express();
 var PORT = 8080;
+
+// Add methods to handle API requests
+api(app);
 
 app.get('/', function (req, res) {
   res.send('Channel list.');
