b34289ece871821a96064df55e0844403385204d
Antonio Vicente Martin <antoniovicente.avm@gmail.com>
2016-02-18 13:21:39 +0100

    Add basic express web service

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 'use strict';
+
+var express = require('express');
+var app = express();
+
+var PORT = 8080;
+
+app.get('/', function (req, res) {
+  res.send('Channel list.');
+});
+
+app.listen(PORT, function () {
+  console.log('Tracker listening on port ' + PORT);
+});
