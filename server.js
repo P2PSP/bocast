9eed63be0805ae29c5dc1adac0e971515fea215d
Jorge García Hinestrosa <jorgegarciahinestrosa@gmail.com>
2016-02-18 20:01:31 +0100

    Add a PUT method to add new channels

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 'use strict';
 
 var express = require('express');
+var bodyParser = require('body-parser');
 var api = require('./lib/api.js');
 
 var app = express();
 var PORT = 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Add methods to handle API requests
 api(app);
